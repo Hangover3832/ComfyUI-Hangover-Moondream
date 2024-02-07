af1da7a89c9aab19c32538a0959faca31aecd4f3
AlexL <github@leual.ch>
2024-02-07 19:59:45 +0100

    update

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-from .moondream import Moondream
+from .ho_moondream import Moondream
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 
 NODE_CLASS_MAPPINGS = {
-    "Moondream Interrogator": MsKosmos2,
+    "Moondream Interrogator": Moondream,
 }
--- moondream.py
+++ moondream.py
@@ -1,66 +1,107 @@
-'''
-An implementation of the moondream visual LLM
-https://huggingface.co/vikhyatk/moondream1
-
-https://github.com/Hangover3832
-
-Alex
-'''
-
-
-from transformers import AutoModelForCausalLM, CodeGenTokenizerFast as Tokenizer
-from PIL import Image
 import torch
-import gc
-import numpy as np
+from torch import nn
+from .vision_encoder import VisionEncoder
+from .configuration_moondream import MoondreamConfig
+from transformers import PreTrainedModel
+import re
 
-class Moondream:
-    HUGGINGFACE_MODEL_NAMES = ["vikhyatk/moondream1",] # other/newer models can be added here
-    DEVICES = ["cpu", "gpu"] if torch.cuda.is_available() else  ["cpu"]
+from .modeling_phi import PhiForCausalLM
+from .configuration_moondream import PhiConfig
 
-    def __init__(self):
-        self.model = None
-        self.tokenizer = None
-        self.modelname = ""
+class Moondream(PreTrainedModel):
+    config_class = MoondreamConfig
 
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "image": ("IMAGE",),
-                "prompt": ("STRING", {"multiline": False, "default": "Please provide a detailed description of this image."},),
-                "huggingface_model": (s.HUGGINGFACE_MODEL_NAMES, {"default": s.HUGGINGFACE_MODEL_NAMES[0]},),
-                "device": (s.DEVICES, {"default": s.DEVICES[0]},),
-            }
+    def __init__(self, config):
+        super().__init__(config)
+        self.vision_encoder = VisionEncoder()
+
+        if type(config.phi_config) == dict:
+            phi_config = PhiConfig(**config.phi_config)
+        else:
+            phi_config = config.phi_config
+        self.text_model = PhiForCausalLM(phi_config)
+
+    @property
+    def device(self):
+        return self.text_model.device
+
+    def encode_image(self, image):
+        return self.vision_encoder(image)
+
+    def input_embeds(self, prompt, image_embeds, tokenizer):
+        def _tokenize(txt):
+            return tokenizer(
+                txt, return_tensors="pt", add_special_tokens=False
+            ).input_ids.to(self.device)
+
+        text_emb = self.text_model.get_input_embeddings()
+
+        # Add BOS token
+        embeds = []
+        embeds.append(
+            text_emb((torch.tensor([[tokenizer.bos_token_id]], device=self.device)))
+        )
+
+        if "<image>" not in prompt:
+            embeds.append(text_emb(_tokenize(prompt)))
+        else:
+            assert prompt.count("<image>") == 1
+            before, after = prompt.split("<image>")
+            embeds.append(text_emb(_tokenize(f"{before}<image>")))
+            embeds.append(image_embeds.to(self.device))
+            embeds.append(text_emb(_tokenize(f"</image>{after}")))
+
+        return torch.cat(embeds, dim=1)
+
+    def generate(
+        self,
+        image_embeds,
+        prompt,
+        tokenizer,
+        eos_text="<END>",
+        max_new_tokens=128,
+        **kwargs,
+    ):
+        eos_tokens = tokenizer(eos_text, add_special_tokens=False)[0].ids
+
+        generate_config = {
+            "eos_token_id": eos_tokens,
+            "bos_token_id": tokenizer.bos_token_id,
+            "pad_token_id": tokenizer.eos_token_id,
+            "max_new_tokens": max_new_tokens,
+            **kwargs,
         }
 
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("description",)
-    FUNCTION = "interrogate"
-    OUTPUT_NODE = False
-    CATEGORY = "Hangover"
+        with torch.no_grad():
+            inputs_embeds = self.input_embeds(prompt, image_embeds, tokenizer)
+            output_ids = self.text_model.generate(
+                inputs_embeds=inputs_embeds, **generate_config
+            )
 
-    def interrogate(self, image:torch.Tensor, prompt:str, huggingface_model:str, device:str):
-        dev = "cuda" if device.lower() == "gpu" else "cpu"
-        if (self.model == None) or (self.tokenizer == None) or (self.modelname != huggingface_model) or (device != self.device):
-            del self.model
-            del self.tokenizer
-            gc.collect()
-            if (device == "cpu") and torch.cuda.is_available():
-                torch.cuda.empty_cache()
-            print(f"moondream: loading model {huggingface_model}, please stand by....")
-            self.model = AutoModelForCausalLM.from_pretrained(huggingface_model, trust_remote_code=True).to(dev)
-            self.tokenizer = Tokenizer.from_pretrained(huggingface_model)
-            self.modelname = huggingface_model
-            self.device = device
+        return tokenizer.batch_decode(output_ids, skip_special_tokens=True)
 
-        descriptions = ""
-        
-        for im in image:
-            i = 255. * im.cpu().numpy()
-            img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
-            enc_image = self.model.encode_image(img)
-            answer = self.model.answer_question(enc_image, "What is this?", self.tokenizer)
-            descriptions += answer
-        
-        return(descriptions,)
+    def answer_question(
+        self,
+        image_embeds,
+        question,
+        tokenizer,
+        chat_history="",
+        result_queue=None,
+        **kwargs,
+    ):
+        prompt = f"<image>\n\n{chat_history}Question: {question}\n\nAnswer: "
+        answer = self.generate(
+            image_embeds,
+            prompt,
+            eos_text="<END>",
+            tokenizer=tokenizer,
+            max_new_tokens=256,
+            **kwargs,
+        )[0]
+        cleaned_answer = re.sub("<$", "", re.sub("END$", "", answer)).strip()
+
+        # Use the result_queue to pass the result if it is provided
+        if result_queue:
+            result_queue.put(cleaned_answer)
+        else:
+            return cleaned_answer
